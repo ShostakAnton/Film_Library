6b1d385628686b9150874666987ff53e6ae3b0d7
Anton Shostak <60942970+ShostakAnton@users.noreply.github.com>
2020-07-29 01:33:52 +0300

    add movie detail

--- movies/urls.py
+++ movies/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from movies.views import *
 
 urlpatterns = [
-    path('', MoviesView.as_view())
+    path('', MoviesView.as_view()),
+    path('<slug:slug>/', MovieDetailView.as_view(), name='movie-detail')
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,12 +1,35 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
 from .models import *
 
 
-class MoviesView(View):
+# class MoviesView(View):
+#     """Список фильмов"""
+#
+#     def get(self, request):
+#         movies = Movie.objects.all()
+#         return render(request, "movies/movie_list.html", {"movie_list": movies})
+
+class MoviesView(ListView):
     """Список фильмов"""
 
-    def get(self, request):
-        movies = Movie.objects.all()
-        return render(request, "movies/movies.html", {"movie_list": movies})
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+    template_name = 'movies/movie_list.html'
+
+
+# class MovieDetailView(View):
+#     """Полное описание фильма"""
+#
+#     def get(self, request, slug):
+#         movie = Movie.objects.get(url=slug)
+#         return render(request, 'movies/movie_detail.html', {'movie': movie})
+
+class MovieDetailView(DetailView):
+    """Полное описание фильма"""
+
+    model = Movie
+    slug_field = 'url'  # по какому полю искать запись
+    template_name = 'movies/movie_detail.html'
