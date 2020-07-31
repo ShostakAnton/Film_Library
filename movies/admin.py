b359c8a52e149e9b6230b57742351413e15b4fc6
Anton Shostak <60942970+ShostakAnton@users.noreply.github.com>
2020-07-31 20:36:27 +0300

    admin panel

--- movies/admin.py
+++ movies/admin.py
@@ -2,11 +2,84 @@ from django.contrib import admin
 from .models import Category, Genre, Movie, MovieShots, Actor, Rating, RatingStar, Reviews
 
 
-admin.site.register(Category)
-admin.site.register(Genre)
-admin.site.register(Movie)
-admin.site.register(MovieShots)
-admin.site.register(Rating)
-admin.site.register(Actor)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Категории"""
+    list_display = ("id", "name", "url")
+    list_display_links = ("name",)
+
+
+class ReviewInline(admin.TabularInline):    # TabularInline - информация по горизонтали
+# class ReviewInline(admin.StackedInline):      StackedInline - информация по вертикали
+    """Отзывы на странице фильма"""
+    model = Reviews
+    extra = 1       # количество дополнитеьных полей для добавления
+    readonly_fields = ("name", "email")     # доступны только для чтения
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    """Фильмы"""
+    list_display = ("title", "category", "url", "draft")
+    list_filter = ("category", "year")
+    search_fields = ("title", "category__name")
+    inlines = [ReviewInline]            # добавление на админ-страницу другую модель
+    save_on_top = True        # панель сохранения вверху
+    save_as = True      # кнопка "сохранить как новый обьект"
+    list_editable = ("draft",)      # для редактирования полей прамо со списка
+    # fields = ("actors", "directors", "genres", "category")        # какие поля должны отображаться
+    fieldsets = (       # для группировки полей
+        (None, {
+            "fields": (("title", "tagline"),)
+        }),
+        (None, {
+            "fields": ("description", "poster")
+        }),
+        (None, {
+            "fields": (("year", "world_premiere", "country"),)
+        }),
+        ("Actors", {
+            "classes": ("collapse",),       # в свернутом виде
+            "fields": (("actors", "directors", "genres", "category"),)
+        }),
+        (None, {
+            "fields": (("budget", "fees_in_usa", "fess_in_world"),)
+        }),
+        ("Options", {
+            "fields": (("url", "draft"),)
+        }),
+    )
+
+
+@admin.register(Reviews)
+class ReviewAdmin(admin.ModelAdmin):
+    """Отзывы"""
+    list_display = ("name", "email", "parent", "movie", "id")
+    readonly_fields = ("name", "email")  # запретить изменять указаные поля (доступны только для чтения)
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    """Жанры"""
+    list_display = ("name", "url")
+
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    """Актеры"""
+    list_display = ("name", "age")
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    """Рейтинг"""
+    list_display = ("movie", "ip", "star")
+
+
+@admin.register(MovieShots)
+class MovieShotsAdmin(admin.ModelAdmin):
+    """Кадры из фильма"""
+    list_display = ("title", "movie")
+
+
 admin.site.register(RatingStar)
-admin.site.register(Reviews)
