77dc16313c947c34c8e6a537f8684cbc62d07a66
Anton Shostak <60942970+ShostakAnton@users.noreply.github.com>
2020-08-04 23:25:27 +0300

    adding a rating to a movie

--- movies/urls.py
+++ movies/urls.py
@@ -5,6 +5,7 @@ from movies.views import *
 urlpatterns = [
     path('', MoviesView.as_view()),
     path("filter/", FilterMoviesView.as_view(), name='filter'),
+    path("add-rating/", AddStarRating.as_view(), name='add_rating'),
     path("json-filter/", JsonFilterMoviesView.as_view(), name='json_filter'),
     path('<slug:slug>/', MovieDetailView.as_view(), name='movie-detail'),
     path("review/<int:pk>/", AddReview.as_view(), name="add_review"),
